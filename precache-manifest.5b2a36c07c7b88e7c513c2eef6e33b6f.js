a70465c70233d6365b15b281d08c139a82b009c1
chirayurathi <chirayurathi4@gmail.com>
2022-04-08 08:05:07 +0530

    Updates

---
+++ precache-manifest.5b2a36c07c7b88e7c513c2eef6e33b6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "44dedc71129b98c6f6a7",
+    "url": "/static/css/main.00042f7e.chunk.css"
+  },
+  {
+    "revision": "44dedc71129b98c6f6a7",
+    "url": "/static/js/main.44dedc71.chunk.js"
+  },
+  {
+    "revision": "06ed7020fbf189b562b7",
+    "url": "/static/js/1.06ed7020.chunk.js"
+  },
+  {
+    "revision": "72fab5218a2ab09dc93c",
+    "url": "/static/js/2.72fab521.chunk.js"
+  },
+  {
+    "revision": "21a7007f90bff1f3f2c7",
+    "url": "/static/js/runtime~main.21a7007f.js"
+  },
+  {
+    "revision": "ef6c27e33fe7f25e6f1ec256fd2cb13f",
+    "url": "/index.html"
+  }
+];
